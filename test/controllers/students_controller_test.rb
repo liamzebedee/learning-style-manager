cba7cf4025d90adbb428b4ee35d2bfa253caf868
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-12-16 20:48:31 +1000

    new test for student results page

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,18 +1,14 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
-  attr_accessor :default_student
-  def setup
-    @@default_student = {'student' => {
-      :year => 2014,
-      :eq_id => 'ledwa7',
-      :name => 'Liam Edwards-Playne' 
-    }}
-  end
-  
   test "should show student dashboard" do
     # TODO should centralise this test student 'Billy Jo' to a single fixture for both development and testing
     get :dashboard, {:id => students(:billy).id}, {'student'=>{:id=>students(:billy).id}}
     assert_response :success
+  end
+  
+  test "should show student results page" do
+  	get :show, {:id => students(:billy).id}, {'student'=>{:id=>students(:billy).id}}
+  	assert_response :success
   end
 end
